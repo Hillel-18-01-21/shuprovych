05b1a4fac93b5bb22cd2ae01de694cc0fb7d25a3
Stepan Shuprovych <amazingstepa@gmail.com>
2021-02-12 14:26:36 +0200

    changing the sort method

--- hw5/scripts.js
+++ hw5/scripts.js
@@ -1,17 +1,28 @@
 alert("Hello");
-let arr =[];
+let arrLenght =[];
 let mamyNambers = "";
 do{
   mamyNambers = +prompt("how many numbers do you want to sort?");
 } while (mamyNambers < 3);
 for(let i = 1; i <= mamyNambers; i++){
   let namber = +prompt("Please push namber");
-  arr.push(namber);
+  arrLenght.push(namber);
 };
-arr.sort(function(a,b){ 
-  return a - b;
-});
-console.log(arr);
+arraySort(arrLenght);
+function arraySort(arr)      
+{                               
+    let n = arr.length;
+    for (let i = 0; i < n; i++)
+     { let v = arr[ i ], j = i-1;
+       while (j >= 0 && arr[j] > v)
+        { arr[j+1] = arr[j]; j--; }
+        arr[j+1] = v;
+     }                    
+    return arr; 
+};
+
+    console.log(arrLenght);  
+
 
 
 
