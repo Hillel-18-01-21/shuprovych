a0d79ee4e97e2762ae79cbf75f8bf368137de6b8
Stepan Shuprovych <amazingstepa@gmail.com>
2021-02-13 17:39:05 +0200

    start new project

--- hw6/scripts.js
+++ hw6/scripts.js
@@ -1,8 +1,9 @@
 alert("Hello");
-let repeat = true;
-const userNambers = prompt("Enter numbers separated by commas", "1,2,...").split(','); //делим стороку по "," и перобразуем в масив  
-let userNambersArray = userNambers.map(Number);; // создаем и преобразуем в масив чисел
 
+const nambers = prompt("Enter numbers separated by commas", "1,2,...").split(','); //делим стороку по "," и перобразуем в масив  
+let nambersArray = nambers.map(Number);; // создаем и преобразуем в масив чисел
+
+function repeat(){
 const choiceAction = prompt(`
 Select and enter the operation number
   1. Sort ascending
@@ -13,36 +14,31 @@ Select and enter the operation number
 `);
 switch(choiceAction) {
   case '1':
-    userNambersArray = userNambersArray.sort(function(a,b) { return a-b});
+    nambersArray = nambersArray.sort(function(a,b) { return a-b});
     break
   case '2':
-    userNambersArray = userNambersArray.find(e => e % 2 == 1); //!!!!!!!!!!!!! пофиксить
+    nambersArray = nambersArray.filter(e => e % 3 == 0);
     break
   case '3':
     let unionSymbol = prompt('Enter union symbol','');
-    userNambersArray = userNambersArray.join(`${unionSymbol}`);
+    nambersArray = nambersArray.join(`${unionSymbol}`);
     break 
   case '4':
-    userNambersArray = userNambersArray.reduce(function(a, b) {
+    nambersArray = nambersArray.reduce(function(a, b) {
     return Math.max(a, b);
     });
     break
   case '5':
-    userNambersArray = userNambersArray.reduce(function(a, b) {
+    nambersArray = nambersArray.reduce(function(a, b) {
     return Math.min(a, b);
     });
     break
 };
-console.log(userNambersArray);
-if (confirm('Do you want to repeat')) {
- repeat = confirm(`Repeat with these numbers - click Yes
- Repeat with new numbers - click No`)
-}
+console.log(nambersArray);
+};
 
-
-
-
-
-
-
-
+ if (confirm('Do you wont repeat')){
+   if (confirm(`do you want to repeat with the same numbers?`)){
+      repeat();    
+   };
+ };
