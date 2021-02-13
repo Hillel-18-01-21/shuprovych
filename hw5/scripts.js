8b5e138abba693d92ec1b1391c9dc85aa8987a11
Stepan Shuprovych <amazingstepa@gmail.com>
2021-02-13 15:47:15 +0200

    renamed var

--- hw5/scripts.js
+++ hw5/scripts.js
@@ -1,27 +1,25 @@
 alert("Hello");
-let arraySort =[];
-let quantityNambers = "";
+let arrayNambers =[];
+let quantity = "";
 do{
-  quantityNambers = +prompt("how many numbers do you want to sort?");
-} while (quantityNambers < 3);
-for(let i = 1; i <= quantityNambers; i++){
+  quantity = +prompt("how many numbers do you want to sort?");
+} while (quantity < 3);
+for(let i = 1; i <= quantity; i++){
   let namber = +prompt("Please push namber");
-  arraySort.push(namber);
+  arrayNambers.push(namber);
 };
-functionSort(arraySort);
-function functionSort(arr)      
-{                               
+functionSort(arrayNambers);
+function functionSort(arr){                               
     let n = arr.length;
     for (let i = 0; i < n; i++)
-     { let v = arr[ i ], j = i-1;
+    { let v = arr[ i ], j = i-1;
        while (j >= 0 && arr[j] > v)
         { arr[j+1] = arr[j]; j--; }
         arr[j+1] = v;
-     }                    
+    }                    
     return arr; 
 };
-
-    console.log(arraySort);  
+console.log(arrayNambers);  
 
 
 
