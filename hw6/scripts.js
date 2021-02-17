6c669959c73c3970f56f1ea6d0fa41111398b137
Stepan Shuprovych <amazingstepa@gmail.com>
2021-02-17 20:31:18 +0200

    finish

--- hw6/scripts.js
+++ hw6/scripts.js
@@ -1,41 +1,57 @@
-alert("Hello");
-const nambers = prompt("Enter numbers separated by commas", "1,2,...").split(','); //делим стороку по "," и перобразуем в масив  
-let nambersArray = nambers.map(Number);; // создаем и преобразуем в масив чисел
 
-const choiceAction = prompt(`
-Select and enter the operation number
-  1. Sort ascending
-  2. Output multiples of 3
-  3. Glue all numbers into a string (with the specified divider)
-  4. Display the maximum number
-  5. Display the minimum number
-`);
+let nambersArray;
+
+showResult()
+
+function showResult() {
+  let result = (getNumber());
+  console.log(getResult(result))
+  while (confirm('Do you wont repeat')){
+    if (confirm(`do you want to repeat with the same numbers?`)){
+      console.log (getResult(result));
+    }else {
+      getNumber();
+      console.log (getResult(result))
+    };
+  };
+};
+
+function getNumber(){
+  let nambers = prompt("Enter numbers separated by commas", "1,2,...").split(','); //делим стороку по "," и перобразуем в масив 
+  nambersArray = nambers.map(Number); // создаем и преобразуем в масив чисел
+  return nambersArray;
+};
+
+function getResult () { 
+let choiceAction = prompt(`
+  Select and enter the operation number
+    1. Sort ascending
+    2. Output multiples of 3
+    3. Glue all numbers into a string (with the specified divider)
+    4. Display the maximum number
+    5. Display the minimum number
+  `);
 switch(choiceAction) {
   case '1':
-    nambersArray = nambersArray.sort(function(a,b) { return a-b});
+    result = nambersArray.sort((a,b) => a-b);
     break
   case '2':
-    nambersArray = nambersArray.filter(e => e % 3 == 0);
+    result = nambersArray.filter((e) => e % 3 == 0);
     break
   case '3':
     let unionSymbol = prompt('Enter union symbol','');
-    nambersArray = nambersArray.join(`${unionSymbol}`);
+    result = nambersArray.join(`${unionSymbol}`);
     break 
   case '4':
-    nambersArray = nambersArray.reduce(function(a, b) {
+    result = nambersArray.reduce(function(a, b) {
     return Math.max(a, b);
     });
     break
   case '5':
-    nambersArray = nambersArray.reduce(function(a, b) {
+    result = nambersArray.reduce(function(a, b) {
     return Math.min(a, b);
     });
     break
 };
-console.log(nambersArray);
-
- if (confirm('Do you wont repeat')){
-   if (confirm(`do you want to repeat with the same numbers?`)){
-     //туплу.....  
-   };
- };
+ return result;
+};
