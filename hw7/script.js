c0b28aaac7b985ad182c17d8f27c18b33f2bff5e
Stepan Shuprovych <amazingstepa@gmail.com>
2021-02-24 15:32:14 +0200

    fs

---
+++ hw7/script.js
@@ -0,0 +1,5 @@
+const addBtn = document.getElementsByClassName('btn_el');
+const addInp = document.getElementsByClassName('inp_el');
+const addList = document.getElementsByClassName('li_el');
+
+addBtn.addEventListener('click', addElement);
