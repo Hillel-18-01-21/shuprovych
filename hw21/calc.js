a9953a1edc29274f26923c48eb70687777202d64
Stepan Shuprovych <amazingstepa@gmail.com>
2021-04-26 20:12:05 +0300

    hw21

--- hw21/calc.js
+++ hw21/calc.js
@@ -1,18 +1,28 @@
 let _val = 0;
-let set = val => {
-    _val = val;
-}
-let add = (x) =>{ _val += x};
-let mult = (x) =>{ _val *= x};
-let div = (x) =>{ _val /= x};
-let sub = (x) =>{ _val -= x};
+
+let set = (val) => {
+  _val = val;
+};
+
+let add = (x) => {
+  _val += x;
+};
+let mult = (x) => {
+  _val *= x;
+};
+let div = (x) => {
+  _val /= x;
+};
+let sub = (x) => {
+  _val -= x;
+};
 
 module.exports = {
-    set,
-    add,
-    mult,
-    div,
-    sub,
-    getResult:() => _val,
-    printResult:() => console.log(_val)
-}
+  set,
+  add,
+  mult,
+  div,
+  sub,
+  getResult: () => _val,
+  printResult: () => console.log(_val),
+};
